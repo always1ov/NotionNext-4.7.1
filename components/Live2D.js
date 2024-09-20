5f8ac7cf1f23be954bc192d53335a99bdc126a25
always1ov <84728964+always1ov@users.noreply.github.com>
2024-09-20 11:57:32 +0800

    Update Live2D.js
    注释切换主题

--- components/Live2D.js
+++ components/Live2D.js
@@ -35,9 +35,9 @@ export default function Live2D() {
   }, [theme])
 
   function handleClick() {
-    if (petSwitchTheme) {
-      switchTheme()
-    }
+    //if (petSwitchTheme) {
+    //  switchTheme()
+    //}
   }
 
   if (!showPet) {
